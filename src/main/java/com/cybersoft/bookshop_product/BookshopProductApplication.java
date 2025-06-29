87c81017a954254413090e575a65410ddb7a25c6
hoa-nguyen-bk <haveaniceday6174@gmail.com>
2025-06-29 09:12:40 +0700

    bẻoe buoi 7

--- src/main/java/com/cybersoft/bookshop_product/BookshopProductApplication.java
+++ src/main/java/com/cybersoft/bookshop_product/BookshopProductApplication.java
@@ -2,10 +2,11 @@ package com.cybersoft.bookshop_product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @SpringBootApplication
 public class BookshopProductApplication {
-
+	@LoadBalanced
 	public static void main(String[] args) {
 		SpringApplication.run(BookshopProductApplication.class, args);
 	}
