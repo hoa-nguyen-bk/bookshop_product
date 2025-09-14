276c60da6bcea45652eb0dffa9ffe6a68d3a127d
hoa-nguyen-bk <haveaniceday6174@gmail.com>
2025-09-14 10:06:19 +0700

    anh Bình dạy buoi 14

--- src/main/java/com/cybersoft/bookshop_product/controller/ProductController.java
+++ src/main/java/com/cybersoft/bookshop_product/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/prod/api/products")
+@RequestMapping("/products/api/products")
 public class ProductController {
     @Autowired
     private ProductService productService;
